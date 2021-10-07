a2ed077cf7cbb274688f9c253d5ba1a5e0364822
Kangcheng Hou <kangchenghou@gmail.com>
2021-10-07 16:25:26 -0700

    add test

---
+++ README.md
@@ -0,0 +1,8 @@
+# Installation
+
+```bash
+# TODO: put these into setup.py
+pip install --upgrade cython numpy
+git clone https://github.com/KangchengHou/xarray-pgen.git
+cd xarray-pgen; pip install -e .
+```
--- xrpgen/tests/test_read.py
+++ xrpgen/tests/test_read.py
@@ -1,8 +1,30 @@
 import xrpgen
 import numpy as np
+import pandas_plink
+import time
 
 
 def test_phase():
-    d1 = xrpgen.read_pgen(xrpgen.get_test_data("phased.pgen"), phase=False)
-    d2 = xrpgen.read_pgen(xrpgen.get_test_data("phased.pgen"), phase=True)
+    path = xrpgen.get_test_data("phased.pgen")
+    d1 = xrpgen.read_pgen(path, phase=False)
+    d2 = xrpgen.read_pgen(path, phase=True)
     assert np.all(d1 == d2.sum(axis=2)).compute()
+
+
+def test_pandas_plink_consistency():
+    path = xrpgen.get_test_data("plink1.bed")
+    d1 = pandas_plink.read_plink1_bin(path, ref="a0", verbose=False).data
+    d2 = xrpgen.read_pgen(path, phase=False)
+    assert np.allclose(d1, d2, equal_nan=True)
+
+
+def test_benchmark_pandas_plink(benchmark):
+    path = xrpgen.get_test_data("plink1.bed")
+    dset = pandas_plink.read_plink1_bin(path, ref="a0", verbose=False).data
+    benchmark(dset.compute)
+
+
+def test_benchmark_xrpgen(benchmark):
+    path = xrpgen.get_test_data("plink1.bed")
+    dset = xrpgen.read_pgen(path, phase=False)
+    benchmark(dset.compute)
