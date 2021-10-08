ced657b027075ccff263d6c37ba4be65209b409e
Kangcheng Hou <kangchenghou@gmail.com>
2021-10-07 19:09:01 -0700

    update tests

--- xrpgen/tests/test_read.py
+++ xrpgen/tests/test_read.py
@@ -18,13 +18,25 @@ def test_pandas_plink_consistency():
     assert np.allclose(d1, d2, equal_nan=True)
 
 
+# TODO: download some large plink1 data sets
+# bencmark between pandas_plink and xrpgen
+
+# TODO: benchmark within xrpgen for plink1 and plink2 files.
+
+
 def test_benchmark_pandas_plink(benchmark):
-    path = xrpgen.get_test_data("plink1.bed")
+    path = xrpgen.get_test_data("1000G.EUR.QC.22.bed")
     dset = pandas_plink.read_plink1_bin(path, ref="a0", verbose=False).data
-    benchmark(dset.compute)
+    benchmark(dset[:, 0:5000].compute)
 
 
-def test_benchmark_xrpgen(benchmark):
-    path = xrpgen.get_test_data("plink1.bed")
+def test_benchmark_xrpgen_plink1(benchmark):
+    path = xrpgen.get_test_data("1000G.EUR.QC.22.bed")
     dset = xrpgen.read_pgen(path, phase=False)
-    benchmark(dset.compute)
+    benchmark(dset[:, 0:5000].compute)
+
+
+def test_benchmark_xrpgen_plink2(benchmark):
+    path = xrpgen.get_test_data("1000G.EUR.QC.22.pgen")
+    dset = xrpgen.read_pgen(path, phase=False)
+    benchmark(dset[:, 0:5000].compute)
